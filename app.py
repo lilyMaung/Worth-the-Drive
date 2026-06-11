ff8429e57bd4723a06da093a7d105684076fe382
lilyMaung <lilymaung3@gmail.com>
2026-06-10 17:56:58 -0700

    Added the render URL

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000',
                    #will update after vercel deployment
                    'https://worth-the-drive-lily.vercel.app',
-
+                    'https://worth-the-drive-lily.onrender.com'
                    
                    ])
 
